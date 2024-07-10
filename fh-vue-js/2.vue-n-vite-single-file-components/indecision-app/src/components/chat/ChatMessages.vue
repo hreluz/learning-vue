1b6db7fa139c08091ff8d5959189c4723d54df1f
Hector <hmreluz@gmail.com>
2024-07-10 10:12:15 -0500

    fh-vue-js: vue+vite: single file components: indecision app - references to HTML elements

--- fh-vue-js/2.vue-n-vite-single-file-components/indecision-app/src/components/chat/ChatMessages.vue
+++ fh-vue-js/2.vue-n-vite-single-file-components/indecision-app/src/components/chat/ChatMessages.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-1 overflow-y-auto p-4">
+  <div ref="chatRef" class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
       <ChatBubble v-for="message in messages" :key="message.id" v-bind="message" />
@@ -9,10 +9,21 @@
 <script setup lang="ts">
 import ChatBubble from './ChatBubble.vue'
 import type { ChatMessage } from '../../interfaces/chat-message.interface';
-
+import { ref, watch } from 'vue';
 interface Props {
   messages: ChatMessage[];
 }
 
-defineProps<Props>();
+const { messages } = defineProps<Props>();
+
+const chatRef = ref<HTMLDivElement | null>(null)
+
+watch(messages, () => {
+  setTimeout(() => {
+    chatRef.value?.scrollTo({
+      top: chatRef.value.scrollHeight,
+      behavior: 'smooth'
+    })
+  }, 100)
+})
 </script>
