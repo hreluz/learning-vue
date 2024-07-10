ddcfbf9c8dc6d68f3833d85a63c2154726e4a0fa
Hector <hmreluz@gmail.com>
2024-07-09 23:29:08 -0500

    fh-vue-js: start of project: 1.intro to vue.js - reactive variables

--- fh-vue-js/1.intro-vue.js/app.js
+++ fh-vue-js/1.intro-vue.js/app.js
@@ -2,9 +2,20 @@ const {createApp, ref} = Vue;
 
 const app = createApp({
     template: `
-        <h1> Hello World</h1>
+        <h1> Hello {{ message }}</h1>
         <p> From app.js</p>
-    `
+    `,
+    setup() {
+        const message = ref("I'm Batman")
+
+        setTimeout(() => {
+            message.value = "I'm Superman"
+        }, 1000)
+
+        return {
+            message
+        }
+    }
 });
 
 app.mount("#myApp")
