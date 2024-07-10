a8509399516a381aea93c06a1eb9b67d4c5fef68
Hector <hmreluz@gmail.com>
2024-07-09 23:33:06 -0500

    fh-vue-js: start of project: 1.intro to vue.js - split events and html

--- fh-vue-js/1.intro-vue.js/app.js
+++ fh-vue-js/1.intro-vue.js/app.js
@@ -1,19 +1,19 @@
 const {createApp, ref} = Vue;
 
 const app = createApp({
-    template: `
-        <h1> Hello {{ message }}</h1>
-        <p> From app.js</p>
-    `,
     setup() {
         const message = ref("I'm Batman")
+        const author = ref("Bruce Wayne")
 
-        setTimeout(() => {
-            message.value = "I'm Superman"
-        }, 1000)
+        const changeQuote = () => {
+            message.value = "Hello, I am Superman"
+            author.value = "Clark Kent"
+        }
 
         return {
-            message
+            message,
+            author,
+            changeQuote
         }
     }
 });
