f3f09012112cc18cb6b2fb66f093936bbf904fca
Hector <hmreluz@gmail.com>
2024-07-10 07:42:33 -0500

    fh-vue-js: vue+vite: single file components: indecision app - split SFC logic

--- fh-vue-js/2.vue-n-vite-single-file-components/indecision-app/src/App.vue
+++ fh-vue-js/2.vue-n-vite-single-file-components/indecision-app/src/App.vue
@@ -4,9 +4,11 @@
   <hr />
   <MyCounter :value="5" />
   <MyCounterScript :value="7" />
+  <MyCounterScript2 :value="9" />
 </template>
 
 <script lang="ts" setup>
 import MyCounter from './components/MyCounter.vue'
 import MyCounterScript from './components/MyCounterScript.vue'
+import MyCounterScript2 from './components/my-counter-script/MyCounterScript2.vue'
 </script>
