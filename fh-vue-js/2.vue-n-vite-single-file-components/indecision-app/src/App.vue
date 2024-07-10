5b0af5b6f8bf9c46e1cf04fa0277a9bdbad44ca8
Hector <hmreluz@gmail.com>
2024-07-10 08:47:14 -0500

    fh-vue-js: vue+vite: single file components: indecision app - structure and chat design

--- fh-vue-js/2.vue-n-vite-single-file-components/indecision-app/src/App.vue
+++ fh-vue-js/2.vue-n-vite-single-file-components/indecision-app/src/App.vue
@@ -1,16 +1,7 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-
-  <hr />
-  <MyCounter :value="5" />
-  <MyCounterScript :value="7" />
-  <MyCounterScript2 :value="9" />
+  <IndecisionView />
 </template>
 
 <script lang="ts" setup>
-import MyCounter from './components/MyCounter.vue'
-import MyCounterScript from './components/MyCounterScript.vue'
-import MyCounterScript2 from './components/my-counter-script/MyCounterScript2.vue'
+import IndecisionView from '@/views/IndecisionView.vue';
 </script>
