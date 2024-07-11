69737ffff19bc541b6778cf4a87567ebae0230c8
Hector <hmreluz@gmail.com>
2024-07-11 13:12:08 -0500

    fh-vue-js: indecision-app: intro to unit test and integration - test on sum array

--- fh-vue-js/2.vue-n-vite-single-file-components/indecision-app/tests/helpers/sum.test.ts
+++ fh-vue-js/2.vue-n-vite-single-file-components/indecision-app/tests/helpers/sum.test.ts
@@ -1,6 +1,19 @@
 import { expect, test } from 'vitest';
-import { sum } from '../../src/helpers/sum';
+import { addArray, sum } from '../../src/helpers/sum';
+import { describe } from 'node:test';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('add function', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
+
+describe('addArray function', () => {
+  test('should return 0 if the array is empty', () => {
+    expect(addArray([])).toBe(0);
+  });
+
+  test('should return 6 if the array is 1,2,3', () => {
+    expect(addArray([1, 2, 3])).toBe(6);
+  });
 });
