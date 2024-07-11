b02d2977322d4808bd117229bf6367d2af22cb22
Hector <hmreluz@gmail.com>
2024-07-11 16:15:13 -0500

    fh-vue-js: indecision-app: intro to unit test and integration - trigger events

--- fh-vue-js/2.vue-n-vite-single-file-components/indecision-app/tests/components/MyCounter.test.ts
+++ fh-vue-js/2.vue-n-vite-single-file-components/indecision-app/tests/components/MyCounter.test.ts
@@ -30,4 +30,39 @@ describe('<MyCounter />', () => {
     expect(counterLabel.text()).toContain(`Counter: ${value}`);
     expect(squareLabel.text()).toContain(`Square: ${value * value}`);
   });
+
+  test('increments the counter when +1 button is clicked', async () => {
+    const value = 5;
+    const wrapper = mount(MyCounter, {
+      props: {
+        value: value,
+      },
+    });
+
+    const btnIncrement = wrapper.find('button');
+    await btnIncrement.trigger('click');
+
+    const [counterLabel, squareLabel] = wrapper.findAll('h3');
+
+    expect(counterLabel.text()).toContain(`Counter: ${value + 1}`);
+    expect(squareLabel.text()).toContain(`Square: ${(value + 1) * (value + 1)}`);
+  });
+
+  test('decrement the counter when -1 button is clicked twice', async () => {
+    const value = 5;
+    const wrapper = mount(MyCounter, {
+      props: {
+        value: value,
+      },
+    });
+
+    const [, btnDecrement] = wrapper.findAll('button');
+    await btnDecrement.trigger('click');
+    await btnDecrement.trigger('click');
+
+    const [counterLabel, squareLabel] = wrapper.findAll('h3');
+
+    expect(counterLabel.text()).toContain(`Counter: ${value - 2}`);
+    expect(squareLabel.text()).toContain(`Square: ${(value - 2) * (value - 2)}`);
+  });
 });
