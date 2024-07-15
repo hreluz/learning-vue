d14ca7f815a5ecfbde5b2b62004a811719fe9645
Hector <hmreluz@gmail.com>
2024-07-15 12:12:18 -0500

    fh-vue-js: indecision-app: intro to unit test and integration - test scroll is triggered

--- fh-vue-js/2.vue-n-vite-single-file-components/indecision-app/tests/components/ChatMessages.test.ts
+++ fh-vue-js/2.vue-n-vite-single-file-components/indecision-app/tests/components/ChatMessages.test.ts
@@ -1,6 +1,7 @@
 import ChatMessages from '@/components/chat/ChatMessages.vue';
 import type { ChatMessage } from '@/interfaces/chat-message.interface';
 import { mount } from '@vue/test-utils';
+import ChatBubble from '@/components/chat/ChatBubble.vue';
 
 const messages: ChatMessage[] = [
   { id: 1, message: 'Hello', itsMine: true },
@@ -15,5 +16,28 @@ describe('<ChatMessages />', () => {
 
     const chatBubbles = wrapper.findAllComponents({ name: 'ChatBubble' });
     expect(chatBubbles.length).toBe(messages.length);
+  });
+
+  test('scrolls down to the bottom after messages update', async () => {
+    const scrollToMock = vi.fn();
+
+    const wrapper = mount(ChatMessages, {
+      props: { messages },
+    });
+
+    const chatRef = wrapper.vm.$refs.chatRef as HTMLDivElement;
+    chatRef.scrollTo = scrollToMock;
+
+    await wrapper.setProps({
+      messages: [...messages, { id: 3, message: 'Hey', itsMine: true }],
+    });
+
+    await new Promise((r) => setTimeout(r, 150));
+
+    expect(scrollToMock).toHaveBeenCalled();
+    expect(scrollToMock).toHaveBeenCalledWith({
+      behavior: 'smooth',
+      top: expect.any(Number),
+    });
   });
 });
