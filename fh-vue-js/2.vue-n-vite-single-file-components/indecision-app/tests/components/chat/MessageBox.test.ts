58a46d2e57e504b22afa7961bc3f900aafe63141
Hector <hmreluz@gmail.com>
2024-07-15 11:36:30 -0500

    fh-vue-js: indecision-app: intro to unit test and integration - test emits

--- fh-vue-js/2.vue-n-vite-single-file-components/indecision-app/tests/components/chat/MessageBox.test.ts
+++ fh-vue-js/2.vue-n-vite-single-file-components/indecision-app/tests/components/chat/MessageBox.test.ts
@@ -2,12 +2,24 @@ import MessageBox from '@/components/chat/MessageBox.vue';
 import { mount } from '@vue/test-utils';
 
 describe('<MessageBox />', () => {
-  test('renders input and button elements correctly', () => {
-    const wrapper = mount(MessageBox);
+  const wrapper = mount(MessageBox);
 
+  test('renders input and button elements correctly', () => {
     expect(wrapper.html()).toMatchSnapshot();
     expect(wrapper.find('input[type="text"]').exists()).toBeTruthy();
     expect(wrapper.find('button').exists()).toBeTruthy();
     expect(wrapper.find('button svg').exists()).toBeTruthy();
+  });
+
+  test('emits sendMessage event when button is clicked with message value', async () => {
+    const message = 'Hello World';
+
+    await wrapper.find('input[type="text"]').setValue(message);
+
+    await wrapper.find('button').trigger('click');
+
+    expect(wrapper.emitted('sendMessage')?.[0]).toEqual([message]);
+
+    expect(wrapper.vm.message as any).toBe('');
   });
 });
