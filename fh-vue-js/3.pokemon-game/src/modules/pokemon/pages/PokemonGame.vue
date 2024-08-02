cb8bc30591c144bc478d166976d533d37cd3a679
Hector <hmreluz@gmail.com>
2024-08-02 16:53:37 -0500

    fh-vue-js: pokemon-app: PokemonPicture component

--- fh-vue-js/3.pokemon-game/src/modules/pokemon/pages/PokemonGame.vue
+++ fh-vue-js/3.pokemon-game/src/modules/pokemon/pages/PokemonGame.vue
@@ -1,13 +1,13 @@
 <template>
-  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-if="isLoading || randomPokemon.id === null"
+    class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="text-3xl">Wait please ...</h1>
     <h3 class="animate-pulse">Loading pokemons ...</h3>
   </section>
 
   <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="m-5">What pokemon is?</h1>
-    <h3>{{ randomPokemon }}</h3>
-    <PokemonPicture />
+    <PokemonPicture :pokemon-id="randomPokemon.id" :show-pokemon="gameStatus != GameStatus.Playing" />
     <PokemonOptions />
 
 
@@ -18,8 +18,9 @@
 import PokemonPicture from '../components/PokemonPicture.vue';
 import PokemonOptions from '../components/PokemonOptions.vue';
 import { usePokemonGame } from '../composables/usePokemonGame';
+import { GameStatus } from '../interfaces';
 
 
-const { gameStatus, randomPokemon } = usePokemonGame()
+const { gameStatus, randomPokemon, isLoading } = usePokemonGame()
 
 </script>
