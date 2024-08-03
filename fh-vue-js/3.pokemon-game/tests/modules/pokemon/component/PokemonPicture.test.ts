a55e8a70a5de762ef125c178c6cbee41e5126638
Hector <hmreluz@gmail.com>
2024-08-03 09:56:12 -0500

    fh-vue-js: pokemon-app: test - on PokemonPicture

---
+++ fh-vue-js/3.pokemon-game/tests/modules/pokemon/component/PokemonPicture.test.ts
@@ -0,0 +1,56 @@
+import PokemonPicture from '@/modules/pokemon/components/PokemonPicture.vue';
+import { mount } from '@vue/test-utils';
+
+describe('<PokemonPicture />', () => {
+  const imageSource = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg`;
+
+  test('should match snapshot', () => {
+    const wrapper = mount(PokemonPicture, {
+      props: {
+        showPokemon: false,
+      },
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  test('should show pokemon image', () => {
+    const wrapper = mount(PokemonPicture, {
+      props: {
+        pokemonId: 25,
+        showPokemon: true,
+      },
+    });
+
+    const image = wrapper.find('img');
+
+    expect(image.attributes('src')).toBe(imageSource);
+
+    expect(image.attributes()).toEqual(
+      expect.objectContaining({
+        class: 'fade-in h-[200px]',
+        src: imageSource,
+        alt: '',
+      }),
+    );
+  });
+
+  test('should render the hidden image when showPokemon prop is false', () => {
+    const wrapper = mount(PokemonPicture, {
+      props: {
+        showPokemon: false,
+      },
+    });
+
+    expect(wrapper.find('img').classes('brightness-0')).toBe(true);
+  });
+
+  test('should render the hidden image when showPokemon prop is false', () => {
+    const wrapper = mount(PokemonPicture, {
+      props: {
+        showPokemon: true,
+      },
+    });
+
+    expect(wrapper.find('img').classes('fade-in')).toBe(true);
+  });
+});
