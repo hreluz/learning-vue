cc7cd07a96abac2d3d386561200e9a90de91a094
Hector <hmreluz@gmail.com>
2024-08-05 11:04:29 -0500

    fh-vue-js: pokemon-app: test - axios mock adapter

--- fh-vue-js/3.pokemon-game/tests/modules/pokemon/composables/usePokemonGame.test.ts
+++ fh-vue-js/3.pokemon-game/tests/modules/pokemon/composables/usePokemonGame.test.ts
@@ -2,6 +2,15 @@ import { usePokemonGame } from '@/modules/pokemon/composables/usePokemonGame';
 import { withSetup } from '../../../utils/with-setup';
 import { GameStatus } from '@/modules/pokemon/interfaces';
 import { flushPromises } from '@vue/test-utils';
+import MockAdapter from 'axios-mock-adapter';
+import { pokemonApi } from '@/modules/pokemon/api/pokemonApi';
+import { pokemonListFake } from '../../../data/fake-pokemons';
+
+const mockPokemonAPi = new MockAdapter(pokemonApi);
+
+mockPokemonAPi.onGet('/?limit=151').reply(200, {
+  results: pokemonListFake,
+});
 
 describe('usePokemon', () => {
   test('should initialize with the correct default values', async () => {
@@ -12,8 +21,8 @@ describe('usePokemon', () => {
     expect(results.pokemonOptions.value).toEqual([]);
     expect(results.randomPokemon.value).toBe(undefined);
 
-    await new Promise((r) => setTimeout(r, 1000));
     await flushPromises();
+
     expect(results.isLoading.value).toBe(false);
     expect(results.pokemonOptions.value.length).toBe(4);
     expect(results.randomPokemon.value).toEqual({
