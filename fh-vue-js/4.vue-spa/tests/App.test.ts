11613800e73f571b2eb51a9dfbf65438936ef08f
Hector <hmreluz@gmail.com>
2024-09-05 18:36:44 -0500

    fh-vue-js: vue-spa: TEST: test in App and LandingLayout

---
+++ fh-vue-js/4.vue-spa/tests/App.test.ts
@@ -0,0 +1,17 @@
+import App from '@/App.vue';
+import router from '@/router';
+import { mount, shallowMount } from '@vue/test-utils';
+
+describe('<App />', () => {
+  test('should render correctly', () => {
+    const wrapper = shallowMount(App, {
+      global: {
+        plugins: [router],
+      },
+    });
+
+    const routerView = wrapper.findComponent({ name: 'RouterView' });
+
+    expect(routerView.exists()).toBe(true);
+  });
+});
