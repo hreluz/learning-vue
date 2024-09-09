d85ca6ae8b1a386e344e6d561c8d36f71526aeda
Hector <hmreluz@gmail.com>
2024-09-09 12:41:39 -0500

    fh-vue-js: projects-app: installing daysiUI

--- fh-vue-js/5.projects-app/src/App.vue
+++ fh-vue-js/5.projects-app/src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <button class="btn">Button</button>
+  <button class="btn btn-neutral">Neutral</button>
+  <button class="btn btn-primary">Primary</button>
+  <button class="btn btn-secondary">Secondary</button>
+  <button class="btn btn-accent">Accent</button>
+  <button class="btn btn-ghost">Ghost</button>
+  <button class="btn btn-link">Link</button>
   <RouterView />
 </template>
