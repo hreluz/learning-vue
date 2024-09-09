7c6ac07b3a52e0c7dfa909ee2d19cefa9b41fba0
Hector <hmreluz@gmail.com>
2024-09-09 13:21:55 -0500

    fh-vue-js: projects-app: project view structure

---
+++ fh-vue-js/5.projects-app/src/modules/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col">
+    <top-menu></top-menu>
+    <main class="flex flex-row">
+      <side-menu></side-menu>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import TopMenu from '@/modules/common/components/TopMenu.vue'
+import SideMenu from '@/modules/projects/components/SideMenu.vue'
+</script>
