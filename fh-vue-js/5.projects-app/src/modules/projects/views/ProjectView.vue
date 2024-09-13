2673d8783b24b7be02af15998f5fd38a2b96c5e9
Hector <hmreluz@gmail.com>
2024-09-13 11:51:51 -0500

    fh-vue-js: projects-app: redirect if project does not exists

--- fh-vue-js/5.projects-app/src/modules/projects/views/ProjectView.vue
+++ fh-vue-js/5.projects-app/src/modules/projects/views/ProjectView.vue
@@ -1,8 +1,46 @@
 <template>
-  <h1 class="text-3x">
-    <BreadCrumbs :name="project?.name ?? 'No name'" />
-  </h1>
-
+  <div class="w-full">
+    <section class="m-2">
+      <bread-crumbs :name="project?.name ?? 'No name'" />
+    </section>
+    <section class="m-2">
+      <div class="overflow-x-auto">
+        <table class="table">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th class="w-14">Done</th>
+              <th>Task</th>
+              <th>Done at</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- row 1 -->
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+            </tr>
+            <!-- row 2 -->
+            <tr class="hover">
+              <th>2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+            </tr>
+            <tr>
+              <th></th>
+              <td>
+                <input type="text"
+                  class="input input-primary w-full opacity-60 transition-all hover:opacity-100 focus:opacity-100"
+                  placeholder="New task">
+              </td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -10,6 +48,7 @@ import BreadCrumbs from '@/modules/common/components/BreadCrumbs.vue';
 import { useProjecstStore } from '../store/project.store';
 import { ref, watch } from 'vue';
 import type { Project } from '../interfaces/project.interface';
+import { useRouter } from 'vue-router';
 
 // import { useRoute } from 'vue-router';
 interface Props {
@@ -18,6 +57,8 @@ interface Props {
 
 // const route = useRoute()
 // console.log(route.params.id);
+
+const router = useRouter();
 const props = defineProps<Props>()
 
 const projectStore = useProjecstStore()
@@ -27,6 +68,10 @@ const project = ref<Project | null>();
 
 watch(() => props.id, () => {
   project.value = projectStore.projectsList.find(p => p.id == props.id);
+
+  if (!project.value) {
+    router.replace('/')
+  }
 }, {
   immediate: true
 })
