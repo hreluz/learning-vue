5fcbec166c6e240a7626684db3fb009868420f0a
Hector <hmreluz@gmail.com>
2025-01-09 14:35:38 -0500

    fh-vue-js: projects-app: Test in the Main.ts

--- fh-vue-js/5.projects-app/tests/main.test.ts
+++ fh-vue-js/5.projects-app/tests/main.test.ts
@@ -1,5 +1,38 @@
-describe('main', () => {
-  test('should work', () => {
-    expect(1).toBe(1);
+import { createPinia } from 'pinia';
+
+vi.mock('@/router', () => ({
+  default: 'router',
+}));
+
+describe('Main.ts', () => {
+  const vue = require('vue');
+
+  const useSpy = vi.fn();
+  const mountSpy = vi.fn();
+
+  const createApp = vi.fn().mockReturnValue({
+    use: useSpy,
+    mount: mountSpy,
+  });
+
+  vi.mock('pinia', async (importOriginal) => {
+    const mod: any = await importOriginal();
+
+    return {
+      ...mod,
+      createPinia: vi.fn().mockReturnValue('pinia'),
+    };
+  });
+
+  vue.createApp = createApp;
+
+  test('should be configured with pinia and router', async () => {
+    await import('@/main');
+
+    expect(vue.createApp).toHaveBeenCalled();
+    expect(mountSpy).toHaveBeenCalledWith('#app');
+
+    expect(useSpy).toHaveBeenCalledWith('router');
+    expect(useSpy).toHaveBeenCalledWith('pinia');
   });
 });
