fd037be823df0e8be7b306f18932b266c6ec8088
Hector <hmreluz@gmail.com>
2025-01-21 15:08:36 -0500

    fh-vue-js: projects-app: Test on FabButton - Slots

--- fh-vue-js/5.projects-app/tests/ modules/common/components/FabButton.test.ts
+++ fh-vue-js/5.projects-app/tests/ modules/common/components/FabButton.test.ts
@@ -1,5 +1,6 @@
 import FabButton from '@/modules/common/components/FabButton.vue';
 import { shallowMount } from '@vue/test-utils';
+import { wrap } from 'module';
 
 describe('<FabButton />', () => {
   test('renders with default position', () => {
@@ -22,5 +23,17 @@ describe('<FabButton />', () => {
     const button = wrapper.find('button');
 
     expect(button.classes()).toContain('top-right');
+  });
+
+  test('renders slot content inside button', () => {
+    const wrapper = shallowMount(FabButton, {
+      slots: {
+        default: '<span>Hola</span>',
+      },
+    });
+
+    const slotContent = wrapper.find('button span');
+    expect(slotContent.exists()).toBe(true);
+    expect(slotContent.text()).toBe('Hola');
   });
 });
