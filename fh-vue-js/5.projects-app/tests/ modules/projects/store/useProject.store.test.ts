452193cb12667d89cd87e6b962a0a4d14eec0e25
Hector <hmreluz@gmail.com>
2025-01-21 20:25:39 -0500

    fh-vue-js: projects-app: Test on Pinia Store - Add Project and LocalStorage

--- fh-vue-js/5.projects-app/tests/ modules/projects/store/useProject.store.test.ts
+++ fh-vue-js/5.projects-app/tests/ modules/projects/store/useProject.store.test.ts
@@ -1,5 +1,6 @@
 import { useProjecstStore } from '@/modules/projects/store/project.store';
 import { createPinia, setActivePinia } from 'pinia';
+import { fakeProjects } from '../../../mocks/projects.fake';
 
 describe('useProjectsStore', () => {
   beforeEach(() => {
@@ -26,5 +27,33 @@ describe('useProjectsStore', () => {
     expect(addTaskToProject).toBeInstanceOf(Function);
     expect(addProject).toBeInstanceOf(Function);
     expect(toggleTask).toBeInstanceOf(Function);
+  });
+
+  test('add a project - action', () => {
+    const store = useProjecstStore();
+
+    store.addProject('New Project');
+
+    expect(store.projects.length).toBe(1);
+    expect(store.projects[0]).toEqual({
+      id: expect.any(String),
+      name: 'New Project',
+      tasks: [],
+    });
+  });
+
+  test('should load from localStorage', () => {
+    localStorage.setItem('projects', JSON.stringify(fakeProjects));
+
+    const store = useProjecstStore();
+    const [project1, project2] = store.projects;
+
+    expect(project1).toEqual({
+      ...fakeProjects.at(0),
+      tasks: expect.any(Array),
+    });
+    expect(project2).toEqual(fakeProjects.at(1));
+
+    expect(store.projects.length).toBe(3);
   });
 });
