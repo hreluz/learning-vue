ebae623b9080fb275fa754591512fe677afbf90c
Hector <hmreluz@gmail.com>
2025-01-27 19:48:27 -0500

    fh-vue-js: admin-shop: ShopLayout: shop design

---
+++ fh-vue-js/6.admin-shop/src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- Create By Joker Banny -->
+  <!-- Header Navbar -->
+
+  <TopMenu />
+
+  <HomeView />
+
+  <!-- Footer -->
+  <CustomFooter />
+</template>
+<script lang="ts" setup>
+import CustomFooter from '../components/CustomFooter.vue';
+import TopMenu from '../components/TopMenu.vue';
+import HomeView from '../views/HomeView.vue';
+
+</script>
